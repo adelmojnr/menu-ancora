db8dc9875b0381590cfc5e6c9b61f405871453e0
Adelmo Junior <junior-adelmo@hotmail.com>
2017-02-13 22:20:50 -0300

    adicionando css

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 var altura = 0,
-	destino = 0,
-	velocidade = 10,
- 	scroller = null;
+destino = 0,
+velocidade = 10,
+scroller = null;
 
 function scrollInicio(elementId) {
 	destino = document.getElementById(elementId).offsetTop;
